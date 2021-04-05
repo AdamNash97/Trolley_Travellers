588d3b32b0d030e2834468be780edc2bede5423b
Adam Nash <adamnash3112@gmail.com>
2021-04-05 21:17:01 +0100

    Add all CRUD routes for orders

--- trolleytravellers/orders/routes.py
+++ trolleytravellers/orders/routes.py
@@ -1,10 +1,85 @@
-from flask import Blueprint
-from trolleytravellers.models import Order, Product
+from flask import Blueprint, jsonify, abort, request
+from trolleytravellers import db
+from trolleytravellers.models import Order, OrderSchema
 
 orders = Blueprint('orders', __name__)
 
+order_schema = OrderSchema(many=True)
+
+@orders.route('/order_list', methods=['GET'])
+def list_orders():
+    orders = Order.query.all()
+    output = order_schema.dump(orders)
+    return jsonify({'order' : output})
+    
+@orders.route('/order/<id>', methods=['GET'])
+def list_order():
+    try:
+        order = Order.query.get(id)
+        return order_schema.jsonify(order)
+    except:
+         abort(400)
+
+@orders.route('/add_order', methods=['POST'])
+def new_order():
+    try:
+        order_date = request.json['order_date']
+        customer_id = request.json['customer_id']
+        volunteer_id = request.json['volunteer_id']
+        new_order = Order(order_date=order_date, customer_id=customer_id, volunteer_id=volunteer_id)
+        db.session.add(new_order)
+        db.session.commit()
+        return order_schema.jsonify(new_order)
+    except:
+         abort(400)
+
+@orders.route('/add_multiple_orders', methods=['POST'])
+def new_orders():
+    try:
+        jsonBody = request.get_json()
+        for json_object in jsonBody:
+            order_date = json_object.get('order_date')
+            customer_id = json_object.get('customer_id')
+            volunteer_id = json_object.get('volunteer_id')
+            new_order = Order(order_date=order_date, customer_id=customer_id, volunteer_id=volunteer_id)
+            db.session.add(new_order)
+            db.session.commit()
+            order_schema = OrderSchema()
+            order_schema.jsonify(new_order)
+        orders = Order.query.all()
+        output = order_schema.dump(orders)
+        return jsonify({'# orders in database' : len(output)})
+    except:
+        abort(400)
 
 
+@orders.route('/update_order/<id>', methods=['PUT'])
+def update_order(id):
+    try:
+        order = Order.query.get(id)
+        order_date = request.json['order_date']
+        customer_id = request.json['customer_id']
+        volunteer_id = request.json['volunteer_id']
+    
+        order.order_date = order_date
+        order.customer_id = customer_id
+        order.volunteer_id = volunteer_id
+      
+        db.session.commit()
+
+        return order_schema.jsonify(order)
+    except:
+         abort(404)
+
+@orders.route('/delete_order/<id>', methods=['DELETE'])
+def delete_order(id):
+    try:
+        order = Order.query.get(id)
+        db.session.delete(order)
+        db.session.commit()
+        return order_schema.jsonify({order})
+    except:
+        abort(404)
 
 
     
