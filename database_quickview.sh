2e28ab1767da827a6c52c632f56461efc3048e26
Ozan Caglar <52323924+ozancaglar@users.noreply.github.com>
2021-04-05 21:49:26 +0100

    Edited database_quickview shell script to be more in line with DRY.

--- database_quickview.sh
+++ database_quickview.sh
@@ -1,46 +1,51 @@
 #! /bin/bash
-
 function database_selector {
 
-echo 'Please select database table you would like to view?'
-echo '1: Customer, 2: Volunteer, 3: Product'
+echo 'Which database table would you like to view?'
+echo '1: Customer, 2: Volunteer, 3: Product, 4: Order'
 read -p "Enter choice : " choice
 
 if [ $choice = 1 ]
 	then
-python << EOF
-from trolleytravellers import db, create_app
-app = create_app()
-app.app_context().push()
-from trolleytravellers.models import Customer, Volunteer, Product
-customers = Customer.query.all()
-for customer in customers:
-	print(customer)
-EOF
+table_var="Customer"
+export table_var
+table_query
+
 elif [ $choice = 2 ]
 	then
-python << EOF
-from trolleytravellers import db, create_app
-app = create_app()
-app.app_context().push()
-from trolleytravellers.models import Customer, Volunteer, Product
-volunteers = Volunteer.query.all()
-for volunteer in volunteers:
-	print(volunteer)
-EOF
+table_var="Volunteer"
+export table_var
+table_query
+
 elif [ $choice = 3 ]
-	then
-python << EOF
-from trolleytravellers import db, create_app
-app = create_app()
-app.app_context().push()
-from trolleytravellers.models import Customer, Volunteer, Product
-products = Product.query.all()
-for product in products:
-	print(product)
-EOF
+	then 
+table_var="Product"
+export table_var
+table_query
+
+elif [ $choice = 4 ]
+	then 
+table_var="Order"
+export table_var
+table_query
+
 fi
 echo 'Would you like to view another database table: [y, n]'
+}
+
+function table_query {
+python << EOF
+import os, importlib
+table = os.environ['table_var']
+print(table)
+from trolleytravellers import db, create_app
+app = create_app()
+app.app_context().push()
+from trolleytravellers.models import *
+entities = eval(table).query.all()
+for entity in entities:
+	print(entity)
+EOF
 }
 
 while :
